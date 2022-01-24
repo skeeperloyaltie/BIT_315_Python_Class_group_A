db368782e4ecfac56d31b603e8af5b7bda74e2df
skeeperloyaltie <gugod254@gmail.com>
2022-01-24 09:21:38 +0300

    BIT 315 Group A Class

---
+++ pythn_class_1.py
@@ -0,0 +1,18 @@
+# Type conversion 
+
+# convert an string to an INT
+
+n = input("Enter a number: ")
+
+print(n)
+
+
+# Convert a float to an INT
+
+num_float = float(input("Enter a float number: "))
+
+print(int(num_float))
+
+# cONVERT A STRING TO AN INT
+
+val = "Skeeper"
