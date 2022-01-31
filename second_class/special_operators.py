4defa4d0bdf845e08e0eb13c40abd962ae1e5ace
skeeperloyaltie <gugod254@gmail.com>
2022-01-31 10:12:53 +0300

    speacial operators.

---
+++ second_class/special_operators.py
@@ -0,0 +1,26 @@
+# Special Operators
+
+# identity operators
+# Example
+me = "skeeper"
+me_a = "loyaltie"
+
+print(me is not me_a)
+print(me is me_a)
+
+print(me is me)
+
+print(me is not me)
+
+# Membership 
+
+a = [1, 10, 11, 12, 89 , 4, 34]
+
+print( 5 not in a )
+
+j = [11, 12,5,14, 13, 56,67].append(a)
+
+a = j.__sizeof__()
+
+print(4 in a)
+
