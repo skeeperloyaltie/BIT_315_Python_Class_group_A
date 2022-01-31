923b9c8a8156d49c462ad872de4c1c1bb84be746
skeeperloyaltie <gugod254@gmail.com>
2022-01-31 09:53:10 +0300

    logical

--- second_class/logical_operators.py
+++ second_class/logical_operators.py
@@ -6,23 +6,27 @@
 citizenship = "KENYAN"
 
 
-def check():
-    user_citizenship = str(input("Enter your citizenship: "))
-    age = int(input("Enter your age: "))
-    if user_citizenship == citizenship and age >= 18:
-        print("Kenyan Citizen and age to vote")
-    elif user_citizenship == citizenship and age <= 17:
-        print("Check your age. You still a kiddo")
-    elif user_citizenship != citizenship and age >= 18:
-        print("You not kenyan. You cannot be here. ")
-    elif user_citizenship != citizenship and age <= 17:
-        print("Get out of here")
-    else:
-        print("We dont know you")
+class logical:
+    
+    def check_and():
+        user_citizenship = str(input("Enter your citizenship: ").upper())
+        age = int(input("Enter your age: "))
+        if user_citizenship == citizenship and age >= 18:
+            print("Kenyan Citizen and age to vote")
+        elif user_citizenship == citizenship and age <= 17:
+            print("Check your age. You still a kiddo")
+        elif user_citizenship != citizenship and age >= 18:
+            print("You not kenyan. You cannot be here. ")
+        elif user_citizenship != citizenship and age <= 17:
+            print("Get out of here")
+        else:
+            print("We dont know you")
         
+    def check_or():
+        pass
 if __name__ == '__main__':
     def main():
-        print(check())  
+        print(logical.check_and())  
     main()
-                
+                    
         
